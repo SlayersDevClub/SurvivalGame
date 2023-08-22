d5b617a8d35fd6571369efa5a152c37ada52e77a
h1ggs <68347537+SlayersDevClub@users.noreply.github.com>
2023-08-22 19:24:57 -0400

    Todo List Plugin

---
+++ Assets/Plugins/Tiny Giant Studio/Modular To Do List/Scripts/Editor/Class/Task.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace TinyGiantStudio.ModularToDoLists
+{
+    [System.Serializable]
+    public class Task
+    {
+        public string myName = "New Task Name";
+        public bool hideMyDetails = true;
+
+        public bool addedDescription = false;
+        public string myDescription = "Description";
+
+        public bool addedReference = false;
+        public List<UnityEngine.Object> references = new List<UnityEngine.Object>();
+
+        public bool completed = false;
+        public bool failed = false;
+        public bool ignored = false;
+
+        public TGSTime creationTime;
+        public bool hasDueDate = false;
+        public TGSTime dueDate;
+        public TGSTime completionTime;
+        public TGSTime failedTime;
+        public TGSTime ignoredTime;
+
+        public List<Topic.Tag> tags = new List<Topic.Tag>();
+
+        //editor only
+        public bool editing;
+    }
+}
